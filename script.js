51e24e0759882310fed21d174858a9802947216e
Atom_Machine <91444797+Ussyu@users.noreply.github.com>
2023-09-13 17:23:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,30 @@ writeButton.addEventListener("click", async () => {
   console.log("write button clicked");
   writeLog.textContent = await "please touch the NFC tag!";
   try {
+    const reader = new NDEFReader();
+    await reader.scan();
+    readLog.textContent = "scan started";
+
+    reader.addEventListener("error", () => {
+      console.log("Error");
+    });
+
+    reader.addEventListener("reading", ({ message, serialNumber }) => {
+      console.log(`> Serial Number: ${serialNumber}`);
+      console.log(message);
+      const record = message.records[0];
+      const { data, encoding, recordType } = record;
+      if (recordType === "text") {
+        const textDecoder = new TextDecoder(encoding);
+        const text = textDecoder.decode(data);
+        readLog.textContent = text;
+      }
+    });
+  } catch (error) {
+    readLog.textContent = error;
+  }
+});
+  try {
     let writeText = inputText.value
     console.log(writeText)
     if (!writeText) {
@@ -17,12 +41,11 @@ writeButton.addEventListener("click", async () => {
     }
     const reader = new NDEFReader();
     await reader.write(writeText);
-    writeLog.textContent = `write: ${writeText}`;
+    writeLog.textContent = `write: ${writeText}`+`read: ${readText}`;
   } catch (error) {
     writeLog.textContent = error;
     console.log(error);
   }
-});
 
 readButton.addEventListener("click", async () => {
   readLog.textContent = await "clicked read button";
